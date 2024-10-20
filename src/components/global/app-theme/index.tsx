375f2aaec7799d808c82ecc8253cef1772505ea2
ilssn <ilssn@adswave.co>
2024-10-20 23:04:55 +0800

    upd: format code

--- src/components/global/app-theme/index.tsx
+++ src/components/global/app-theme/index.tsx
@@ -7,7 +7,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 const AppTheme = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex size-full flex-col">
-      <NextThemesProvider defaultTheme='system' attribute="class">{children}</NextThemesProvider>
+      <NextThemesProvider defaultTheme="system" attribute="class">
+        {children}
+      </NextThemesProvider>
     </div>
   );
 };

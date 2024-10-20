4b3c2ebbb4391b856e41ff70ce440612c9c3e71e
ilssn <ilssn@adswave.co>
2024-10-21 02:18:39 +0800

    upd: remove persist config

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,18 +1,9 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
 
 import { ConfigStore, createConfigSlice } from "./slices/config-slice";
 
 type AppStore = ConfigStore;
 
-export const useAppStore = create<AppStore>()(
-  persist(
-    (...a) => ({
-      ...createConfigSlice(...a),
-    }),
-    {
-      name: "app-store",
-      storage: createJSONStorage(() => sessionStorage),
-    }
-  )
-);
+export const useAppStore = create<AppStore>()((...a) => ({
+  ...createConfigSlice(...a),
+}));
